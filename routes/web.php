05a8cc37685db1f0c95a7f7d13d75ea1b65ee151
Prince Chomunorwa <31795158+PrinceChomu@users.noreply.github.com>
2022-10-17 11:20:23 +0200

    template interface

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/', function () {return view('dashboard');});
+
 });
 
 Route::middleware([
