e3b15f5e60b3edd303c2c53f62b6a1224dbee78b
Prince Chomunorwa <31795158+PrinceChomu@users.noreply.github.com>
2022-10-17 11:39:20 +0200

    database migration

---
+++ database/migrations/2022_10_17_092231_create_appointments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->string('patient_id');
+            $table->string('department_id');
+            $table->date('date');
+            $table->time('time');
+            $table->string('doctor');
+            $table->string('status', enum('active', 'inactive'));
+            $table->text('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
