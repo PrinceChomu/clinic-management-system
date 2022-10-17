7cd0dceff9c72e2ae0f19e22969a1829dc15bbef
Prince Chomunorwa <31795158+PrinceChomu@users.noreply.github.com>
2022-10-17 11:48:35 +0200

    migration soft delets

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +18,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Role::create(['name' => 'superadmin']);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'doctor']);
+        Role::create(['name' => 'patient']);
+        Role::create(['name' => 'secretary']);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $admin = User::create([
+            'name' => 'Prince Chomunorwa',
+            'email' => 'pchomu.pc@gmail.com',
+            'phone' => '+263782880047',
+            'password' => Hash::make('prince1997')
+        ]);
+        $admin->assignRole('superadmin');
     }
 }
